704a16d76e3343be37a2d6e5ea6f66b67cf2640c
olivia svensson <oliviateoliva@gmail.com>
2022-11-07 17:57:37 +0100

    make striped columns

--- ovning.js
+++ ovning.js
@@ -5,9 +5,8 @@ let rad4 = document.createElement('h2');
 let rad5 = document.createElement('h1');
 
 let rader = [rad1, rad2, rad3, rad4, rad5];
-
 const bgc = ["#94ff97", "#b3ffb5", "#b3ffd7", "#b3fff1", "#b3e0ff"]
-
+const border = "10px solid #9999ff";
 
 for (let i = 0; i < rader.length; i++) {
     document.body.appendChild(rader[i]);
@@ -24,26 +23,68 @@ document.body.appendChild(boxen);
 boxen.style.border = "1px solid black";
 boxen.style.height = "400px";
 boxen.style.width = "98vw";
+boxen.style.position = "relative"
+boxen.style.display = "flex"
 
-const rektangelett = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let text = " ";
-for (let i = 0; i < rektangelett.length; i++) {
-    text += rektangelett[i] + "<br>";
-}
+//första kolumnen
+const colnr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cont1 = document.createElement("div")
+for (i in colnr1) {
+    const innercol1 = document.createElement("div")
+    cont1.append(innercol1)
+    innercol1.innerText = `${colnr1[i]}`
 
-let r1 = document.createElement("p");
-
-document.body.appendChild(r1);
-
-for (let i = 0; i < rektangelett.length; i++) {
-    let r2 = document.createElement("p");
-    document.body.appendChild(r2);
-    if ((( i + 1 ) % 2 ) > 0 ) {
-        r2.style.backgroundColor = "#000000";
-    } else {
-        r2.style.backgroundColor = "#000000";
+    if (i % 2 === 0) {
+        innercol1.style.backgroundColor = "#000000";
+        innercol1.style.color = "#FFFFFF";
     }
 }
 
-r1.innerHTML = text;
+boxen.append(cont1)
+cont1.style.width = "50px"
+cont1.style.border = border
+cont1.style.margin = "100px"
+
+//andra kolumnen
+const colnr2 = [9,8,7,6,5,4,3,2,1]
+const cont2 = document.createElement("div")
+for (i in colnr2) {
+    const innercol2 = document.createElement("div")
+    cont2.append(innercol2)
+    innercol2.innerText = `${colnr2[i]}`
+
+    if (i % 2 === 0) {
+        innercol2.style.backgroundColor = "#000000";
+        innercol2.style.color = "#FFFFFF";
+    }
+}
+
+
+boxen.append(cont2)
+cont2.style.width = "50px"
+cont2.style.border = border
+cont2.style.margin = "100px"
+
+
+//tredje kolumnen 
+const colnr3 = ["ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio"]
+const cont3 = document.createElement("div")
+for (i in colnr3) {
+    const innercol3 = document.createElement("div")
+    cont3.append(innercol3)
+    innercol3.innerText = `${colnr3[i]}`
+
+    if (i % 2 === 0) {
+        innercol3.style.backgroundColor = "#000000"
+        innercol3.style.color = "#FFFFFF"
+    }
+}
+
+
+boxen.append(cont3)
+cont3.style.width = "50px"
+cont3.style.border = border
+cont3.style.margin = "100px"
+
+
